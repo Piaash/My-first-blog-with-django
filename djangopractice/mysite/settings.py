f728b8d694b8a35f899ee89707d33250a715457b
Piaash <piaash11cse@gmail.com>
2018-03-10 15:55:09 +0600

    Update settings.py

--- djangopractice/mysite/settings.py
+++ djangopractice/mysite/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Piaash$default',
-        'HOST': 'Piaash.mysql.pythonanywhere-services.com',
+        'NAME': 'djangodb',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': 'Piaash',
+        'USER': 'root',
         'PASSWORD': '',
     }
 }
